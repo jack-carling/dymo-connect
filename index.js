0e5f714a8258e28f291285058742a0c366b2e675
jack-carling <jack@carling.se>
2022-06-07 21:38:32 +0200

    GetPrinters

--- index.js
+++ index.js
@@ -17,8 +17,28 @@ class Dymo {
   }
 
   static async getPrinters() {
-    const response = await fetch(`${this.url}/GetPrinters`, { agent });
-    const data = await response.text();
+    try {
+      const response = await fetch(`${this.url}/GetPrinters`, { agent });
+      const data = await response.text();
+
+      const parser = new DOMParser();
+      const xml = parser.parseFromString(data, 'text/xml');
+      const names = xml.getElementsByTagName('Name');
+      const models = xml.getElementsByTagName('ModelName');
+      const connections = xml.getElementsByTagName('IsConnected');
+
+      let result = [];
+      for (let i = 0; i < names.length; i++) {
+        result.push({
+          name: names[i].childNodes[0].nodeValue,
+          model: models[i].childNodes[0].nodeValue,
+          connected: connections[i].childNodes[0].nodeValue === 'True' ? true : false,
+        });
+      }
+      return { success: true, data: result };
+    } catch (e) {
+      return { success: false };
+    }
   }
 }
 
