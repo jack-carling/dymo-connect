2be9dece002be4f49f80b8bac2cb8e3d1137c016
jack-carling <jack@carling.se>
2022-06-07 21:19:13 +0200

    Dymo class and fetch

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const fetch = require('cross-fetch');
+const https = require('https');
+
+const { DOMParser } = require('@xmldom/xmldom');
+
+const agent = new https.Agent({
+  rejectUnauthorized: false,
+});
+
+class Dymo {
+  constructor() {}
+
+  static get url() {
+    return 'https://127.0.0.1:41951/DYMO/DLS/Printing';
+  }
+
+  static async getPrinters() {
+    const response = await fetch(`${this.url}/GetPrinters`, { agent });
+    const data = await response.text();
+  }
+}
+
+module.exports = Dymo;
